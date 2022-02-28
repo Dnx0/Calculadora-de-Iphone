33ecc9e2f971c3b10e1be69a8522ca431f193a5d
Dnx0 <82619763+Dnx0@users.noreply.github.com>
2022-02-28 18:47:08 -0300

    Add files via upload

---
+++ Calculdadora de iphone.js
@@ -0,0 +1,15 @@
+//Titulo
+document.write ("<strong>Divisor de Iphone 1.0</strong>")
+
+//Entrada de dados
+let iphone = prompt (" Quantos Iphone você tem?");
+let pessoas = prompt ("Para divir por quantas pessoas");
+
+// processamento
+let total =  iphone / pessoas ^ 0 ;
+let resto =  iphone % pessoas ^ 0 ;
+
+// mensagem final
+document.write("<br> O total de iphone 📱 é " + iphone)
+document.write ("<br> O total de pessoas 👤 é " + pessoas)
+document.write (" <br> O total de iphone 🍎 por pessoa é " + (total) + " e sobrou " + (resto) + " para você ");
